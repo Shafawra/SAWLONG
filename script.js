5cfaf614da4e480259d7dafdf7fa70d47cbdf7a7
Shafawra <shafaaura35@gmail.com>
2026-02-26 11:41:30 +0800

    upload semua file sawlong

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("halo");
+
+// NAVBAR TOGGLE 
+const toggle = document.getElementById("menu-toggle");
+const nav = document.querySelector(".navbar-nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+
+// FAQ
+const headers = document.querySelectorAll(".faq-header");
+const items = document.querySelectorAll(".faq-item");
+
+headers.forEach(header => {
+    header.addEventListener("click", () => {
+        const item = header.parentElement;
+        items.forEach(i => i.classList.remove("active"));
+
+        item.classList.toggle("active");
+    });
+});
+
